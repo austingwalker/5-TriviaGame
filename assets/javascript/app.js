7a54a96daf7c55345a7bc568ac3b9d9a9b3a77a1
World Wide <agwalker249@gmail.com>
2018-12-01 02:01:34 -0600

    final javascript - not styled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ start.on("click", function(){
     game.loadQuestion();
 });
 
-$(document).on("click", "#answerbtn", function(e){
+$(document).on("click", ".answer-button", function(e){
     game.clicked(e);
 });
 
@@ -44,46 +44,116 @@ var questions = [
 
         countdown: function(){
             game.counter--;
-            $("#counter").html("<h2>TIME REMAINING: " + game.counter + "</h2>");
+            $("#counter").html("<h5>TIME REMAINING: " + game.counter + "</h5>");
             if(game.counter <= 0){
-                alert("Out of time!")
-                this.timesUp();
+                game.timesUp();
 
                 
             }
 
         },
         timesUp: function(){
+            clearInterval(timer);
+            game.unanswered++
+
+            subwrapper.html("<h2>Out of time!</h2>")
+
+            if(game.currentQuestion == questions.length - 1){
+                setTimeout(game.results, 3*1000);
+            }
+
+            else {
+                setTimeout(game.nextQuestion, 3*1000)
+            }
 
         },
         loadQuestion: function(){
-            setInterval(game.countdown, 1000);
+            timer = setInterval(game.countdown, 1000);
 
-            subwrapper.html("<h2>" + game.questions[currentQuestion].question + "<h2>");
+            subwrapper.html("<div id='counter'></div>")
+            subwrapper.append("<h2>" + questions[game.currentQuestion].question + "<h2>");
 
-            for(var i = 0; i < game.questions[currentQuestion].answers; i++){
-            subwrapper.append("<h2 id='answerbtn'data-name='" + game.questions[currentQuestion].answers[i] >" + game.questions[currentQuestion].answers[i] + </h2>")
+            for(var i = 0; i < questions[game.currentQuestion].answers.length; i++){
+                subwrapper.append('<button class="answer-button" id="button-'+i+'" data-name="' + questions[game.currentQuestion].answers[i] + '">' + questions[game.currentQuestion].answers[i] + '</button>')
             }
+
+          
 
 
 
         },
         nextQuestion: function(){
+            game.counter = 30
+            $("#counter").html(game.counter);
+            game.currentQuestion++
+            game.loadQuestion();
 
         },
-        clicked: function(){
+        clicked: function(e){
+            
+            console.log($(e.target).data("name"));
+            console.log(questions[game.currentQuestion].correctAnswer)
+
+            
+
+            clearInterval(timer);
+
+            if($(e.target).data("name")==questions[game.currentQuestion].correctAnswer){
+                game.answeredCorrectly();
+            }
+            else {
+                game.answeredIncorrectly();
+            }
 
         },
-        correctAnswer: function(){
+        answeredCorrectly: function(){
+
+            game.correct++
+            
+            if(game.currentQuestion >= questions.length - 1){
+                game.results()
+            }
+
+            else {
+                clearInterval(timer);
+                subwrapper.html("<h2>Correct!</h2>")
+                setTimeout(game.nextQuestion, 3*1000)
+            }
+            
 
         },
-        incorrectAnswer: function(){
+        answeredIncorrectly: function(){
+
+            game.incorrect++
+
+            if(game.currentQuestion >= questions.length - 1){
+                game.results()
+            }
+
+            else {
+                clearInterval(timer);
+                subwrapper.html("<h2>Incorrect!</h2>")
+                setTimeout(game.nextQuestion, 3*1000)
+            }
 
         },
         results: function(){
 
+            subwrapper.html("<div>Game Over</div>");
+            subwrapper.append("<div>Correct: " + game.correct +"</div>");
+            subwrapper.append("<div>Inorrect: " + game.incorrect +"</div>");
+            subwrapper.append("<div>Unanswered: " + game.unanswered +"</div>");
+            subwrapper.append("<button id='reset'>RESET</div>");
+
         },
         reset: function(){
+
+            game.currentQuestion = 0;
+            game.counter = 30
+            game.correct = 0;
+            game.incorrect = 0;
+            game.unanswered = 0;
+            game.loadQuestion();
 
         }
 
